ee3aafa82511f695d87d55df08ddb58d1599af36
John Mitnick <overkazaf@gmail.com>
2016-02-24 22:56:57 +0800

    update codes

---
+++ src/com/turling/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.turling.activities;
+
+import com.tuling.activities.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
